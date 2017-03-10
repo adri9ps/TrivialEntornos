9f14cdcf96baedcb3768eb3290263349a78b2a45
Adrian Poveda Sanchis <adri9ps@mbp-de-adrian.wifi.alumnado>
2017-03-10 19:23:01 +0100

    holaquetal

--- src/Ficha.java
+++ src/Ficha.java
@@ -4,21 +4,5 @@ public class Ficha {
 	String color= "amarillo";
 	String quesitos = "ciencias";
 	//puto amo
-	int 
-	dfvdfbz
-	sdv
-	sf
-	vsd
-	vxf
-	bsf
-	bsfb
-	sf
-	bsf
-	bsf
-	bsf
-	bsf
-	bs
-	fbsf
-	b
 	
 }
